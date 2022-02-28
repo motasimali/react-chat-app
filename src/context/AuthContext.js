49cca5e2c574c0cf34cbbbee6b4e76be2b318cd8
Motasim Ali Bhatti <motasim.ali@pagos.ai>
2022-02-28 19:42:41 +0500

    user add modal with auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useReducer } from 'react'
+import PropTypes from 'prop-types'
+import AuthReducer from './AuthReducer'
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  isFetching: false,
+  error: false,
+}
+export const AuthContext = createContext(INITIAL_STATE)
+
+AuthContextProvider.propTypes = {
+  /** Text to show on header */
+  children: PropTypes.object.isRequired,
+}
+
+export function AuthContextProvider({ children }) {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE)
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(state.user))
+  }, [state.user])
+
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
